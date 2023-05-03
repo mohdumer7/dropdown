a5aa60bbdf9fcf0ca8ff6e91463a559d42e87798
Mohammed Umer <52200381+mohdumer7@users.noreply.github.com>
2023-05-03 10:48:45 +0530

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [selected, setSelected] = useState("Hello");
+  const [show, setshow] = useState();
+
+  const hovered = () => {
+    setshow(true);
+  };
+  const nonhovered = () => {
+    setshow(false);
+  };
+
+  const clicked = (val) => {
+    setshow(false);
+    setSelected(val);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-full bg-slate-300 app">
+      <div className="flex justify-center items-center flex-col">
+        <h1 className="text-slate-600 mt-10 text-5xl ">DROPDOWN</h1>
+        <select
+          className=" mt-10 transition"
+          onMouseEnter={hovered}
+          onMouseLeave={nonhovered}
         >
-          Learn React
-        </a>
-      </header>
+          <option className="w-[20px]">{selected}</option>
+        </select>
+        <div className="bg-white w-[200px] text-center">
+          {show && (
+            <div>
+              <p
+                onMouseEnter={hovered}
+                onMouseLeave={nonhovered}
+                onClick={() => clicked("Hello")}
+                className="hover:bg-slate-300 cursor-pointer"
+              >
+                Hello
+              </p>
+              <p
+                onMouseEnter={hovered}
+                onMouseLeave={nonhovered}
+                onClick={() => clicked("Hi")}
+                className="hover:bg-slate-300 cursor-pointer"
+              >
+                Hi
+              </p>
+              <p
+                onMouseEnter={hovered}
+                onMouseLeave={nonhovered}
+                onClick={() => clicked("Bye")}
+                className="hover:bg-slate-300 cursor-pointer"
+              >
+                Bye
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
